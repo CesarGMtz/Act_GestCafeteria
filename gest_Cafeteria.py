b8cf50ac572a7b8e8a07d0054f2923589ba1ee52
CsarW <A01656774@tec.mx>
2024-04-02 21:16:40 -0600

    Prueba valSize pasada

--- gest_Cafeteria.py
+++ gest_Cafeteria.py
@@ -7,13 +7,18 @@ def checkName(na):
         return(False)
 
 def checkSizes(si):
-    li = [float(num) if '.' in num else int(num) for num in re.split(r'[-+]?\d*\.\d+|\d+', si)]
-    print(li)
+    # li = [float(num) if '.' in num else int(num) for num in re.split(r"[0-9]+.*[0-9]+.*[0-9]+.*[0-9]+\.[0-9]+.*[0-9]+\.[0-9]+.*[0-9]+\.[0-9]+", si)]
+    li = [num.strip() for num in si.split(",")]
+    lis = []
     for num in li:
-        if (isinstance(num, int) and 0 < num < 49):
-            print(num)
-        else:
+        if ("." in num):
             return(False)
+        else:
+            tNum = int(num)
+            if (isinstance(tNum, int) and 0 < tNum < 49):
+                print(num)
+            else:
+                return(False)
     return(True)
 
 # print("Agrega una nueva Bebida")
--- test_pytest.py
+++ test_pytest.py
@@ -12,3 +12,8 @@ def test_lenName():
     assert gest_Cafeteria.checkName("KawaFresa") == True
     assert gest_Cafeteria.checkName("K") == False
     assert gest_Cafeteria.checkName("KawaFresaMuySabrosa") == False
+    
+def test_valSize():
+    assert gest_Cafeteria.checkSizes("1, 2, 3") == True
+    assert gest_Cafeteria.checkSizes("1, 2, 3.3") == False
+    assert gest_Cafeteria.checkSizes("0, 1, 2, 49") == False
