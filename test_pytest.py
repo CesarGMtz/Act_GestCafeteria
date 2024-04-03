3bc28d01f82ec73bf86558f9db86b6f6575ddfeb
CsarW <A01656774@tec.mx>
2024-04-02 20:24:39 -0600

    Prueba lenName pasada

--- test_pytest.py
+++ test_pytest.py
@@ -7,3 +7,8 @@ def test_onlyAlpha():
     assert gest_Cafeteria.checkName("KawaFresa") == True
     assert gest_Cafeteria.checkName("Kawa Fresa") == True
     assert gest_Cafeteria.checkName("K4w4Fr3s4") == False
+
+def test_lenName():
+    assert gest_Cafeteria.checkName("KawaFresa") == True
+    assert gest_Cafeteria.checkName("K") == False
+    assert gest_Cafeteria.checkName("KawaFresaMuySabrosa") == False
