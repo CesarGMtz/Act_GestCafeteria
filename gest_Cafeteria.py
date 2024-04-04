904c2abebca52ca72a948bc1595d4d0c82374ba7
CsarW <A01656774@tec.mx>
2024-04-04 11:46:14 -0600

    Comentarios

--- gest_Cafeteria.py
+++ gest_Cafeteria.py
@@ -1,52 +1,71 @@
+#Se importa las librerias a usar
 import re
 
+#Función que checa si el formato, de la bebida agregada, es correcto
 def checkFormat(dr):
+    #Obtiene el primer elemento del string de la bebida, presuntamente el nombre de esta
     name = dr.split(",", 1)[0].strip()
+    #Obtiene el resto del string de la bebida, presuntamente la lista de tamaños de esta
     sizes = dr.split(",", 1)[1].strip()
-    if(checkName(name) and checkSizes(sizes)):
-        return(True)
-    else:
-        return(False)
-
-def checkName(na):
-    if (1 < len(na) < 16):
-        return all(char.isalpha() or char.isspace() for char in na)
-    else:
-        return(False)
-
-def checkSizes(si):
-    if (si.isspace() or si == ""):
-        return(False)
     
-    li = re.findall(r'\d+\.\d+|\d+', si)
-    print(li)
-    i = 1
-
-    if (0 < len(li) < 6):
-        while i < len(li):
-            if (li[i] < li[i - 1]):
-                return(False)
-            i += 1
+    #Checa si el string de entrada, el nombre y la lista de tamaños tienen formato correcto
+    if(checkName(name) and checkSizes(sizes)):
+        return(True) #Si sí, devuelve True
     else:
-        return(False)
+        return(False) #Si no, False
 
+
+#Función que checha si el formato, del nombre de la bebida agregada, es correcto
+def checkName(na):
+    #Checa si el string del nombre tiene entre 2 y 15 caracteres
+    if (1 < len(na) < 16):
+        return all(char.isalpha() or char.isspace() for char in na) #Si sí, devuelve True si todos los caracterés del nombre 
+                                                                    #son una letra o un espacio, si no, False 
+    else:
+        return(False) #Si no, False
+
+
+#Función que checa si el formato, de la lista de tamaños de la bebida agregada, es correcto
+def checkSizes(si):
+    #Checa si el string de la lista de tamaños es solamente espacios en blanco o si está vacía
+    if (si.isspace() or si == ""):
+        return(False) #Si sí, devuelve True
+    
+    #Se crea una lista que contiene todos los valores númericos del string
+    li = re.findall(r'\d+\.\d+|\d+', si)
+    
+    i = 1 #Se declara un contador para el ciclo
+    #Checa si la lista tiene entre 1 y 5 elementos
+    if (0 < len(li) < 6):
+        #Si sí, se crea un ciclo que se repetirá por cada elemento de la lista
+        while i < len(li):
+            #Checa si el elemento visto de la lista es menor al que le precede
+            if (li[i] < li[i - 1]):
+                return(False) #Si sí, devuelve False
+            i += 1 #Si no, revisa el elemento siguiente
+    else:
+        return(False) #Si no, False
+
+    #Se crea un ciclo que se repetirá por cada elemento de la lista
     for num in li:
+        #Checa si "." se encuentra en el string del número
         if ("." in num):
-            return(False)
+            return(False) #Si sí, devuelve False
         else:
+            #Si no, se tranforma el string del número a integer
             tNum = int(num)
-            if (isinstance(tNum, int) and 0 < tNum < 49):
-                print(num)
+            #Checa si el valor del tamaño se encuentra entre 1 y 48
+            if (0 < tNum < 49):
+                print(num) #Si sí, imprimimos el número
             else:
-                return(False)
-    return(True)
+                return(False) #Si no, False
+    return(True) #Si no, True
+
 
 # print("Agrega una nueva Bebida")
-# x = input()
+# drink = input()
 
-# name = x.split(",", 1)[0].strip()
-# sizes = x.split(",", 1)[1].strip()
-# print("Nombre: ")
-# print(checkName(name))
-# print("Tamaño: ")
-# print(checkSizes(sizes))
+# if (checkFormat(drink)):
+#     print('Bebida Agregada Exitosamente')
+# else:
+#     print('Proceso Fallido')
--- test_pytest.py
+++ test_pytest.py
@@ -1,43 +1,51 @@
 import gest_Cafeteria
 
-# def test_increment():
-#     assert gest_Cafeteria.increment(3) == 4
-
+#Test que comprueba que, el nombre, solo se componga de caractéres alfabéticos
 def test_onlyAlpha():
-    assert gest_Cafeteria.checkName("KawaFresa") == True
-    assert gest_Cafeteria.checkName("Kawa Fresa") == True
-    assert gest_Cafeteria.checkName("K4w4Fr3s4") == False
+    assert gest_Cafeteria.checkName("KawaFresa") == True #Todos los caractéres son alfabéticos, así que True
+    assert gest_Cafeteria.checkName("Kawa Fresa") == True #El espacio es válido como parte del nombre, así que True
+    assert gest_Cafeteria.checkName("K4w4Fr3s4") == False #Los números no son caractéres alfabéticos, así que False
+    assert gest_Cafeteria.checkName("Kawa Fresa!") == False #El signo ! no es un caractér alfabético, así qeu False
 
+#Test que comprueba que, el nombre, tenga un largo de entre 2 a 15 caractéres
 def test_lenName():
-    assert gest_Cafeteria.checkName("KawaFresa") == True
-    assert gest_Cafeteria.checkName("K") == False
-    assert gest_Cafeteria.checkName("KawaFresaMuySabrosa") == False
+    assert gest_Cafeteria.checkName("KawaFresa") == True #Tiene 9 caractéres, así que True
+    assert gest_Cafeteria.checkName("K") == False #Tiene 1 caractér, así que False
+    assert gest_Cafeteria.checkName("KawaFresaMuySabrosa") == False #Tiene 19 caractéres, así que False
     
+#Test que comprueba que, los valores de tamaño de la lista, tengan valores enteros y sean de 1 a 48
 def test_valSize():
-    assert gest_Cafeteria.checkSizes("1, 2, 3") == True
-    assert gest_Cafeteria.checkSizes("a, b, c") == False
-    assert gest_Cafeteria.checkSizes("1, 2, 3.3") == False
-    assert gest_Cafeteria.checkSizes("0, 1, 2, 49") == False
-    assert gest_Cafeteria.checkSizes("-1, 0, 1, 2") == False
+    assert gest_Cafeteria.checkSizes("1, 2, 3") == True #Todos los valores son enteros, así que True
+    assert gest_Cafeteria.checkSizes("a, 2, c") == True #Los valores no númericos son ignorados y hay un entero, así que True
+    assert gest_Cafeteria.checkSizes("a, b, c") == False #Ni un valor es númerico, así que False
+    assert gest_Cafeteria.checkSizes("1, 2, 3.3") == False #Hay un valor decimal, así que False
+    assert gest_Cafeteria.checkSizes("1, 2, 49") == False #El 49 se sale del rango establecido, así que False
+    assert gest_Cafeteria.checkSizes("-1, 0, 1, 2") == False #El -1 y 0 se salen del rango establecido, así que False
 
+#Test que comprueba que, los valores de tamaño de la lista, esten en orden ascendente
 def test_sizeOrder():
-    assert gest_Cafeteria.checkSizes("1") == True
-    assert gest_Cafeteria.checkSizes("1, 2, 3") == True
-    assert gest_Cafeteria.checkSizes("1, 3, 5") == True
-    #assert gest_Cafeteria.checkSizes("1, 3, 3") == False IDK como manejarlo
-    assert gest_Cafeteria.checkSizes("2, 3, 4, 1") == False
-    assert gest_Cafeteria.checkSizes("3, 2, 1") == False
+    assert gest_Cafeteria.checkSizes("1") == True #Un solo elemento es válido, así que True
+    assert gest_Cafeteria.checkSizes("1, 2, 3") == True #Los elementos están en orden ascendente, así que True
+    assert gest_Cafeteria.checkSizes("1, 3, 5") == True #Los elementos no tienen que ser consecutivos, así que True
+    assert gest_Cafeteria.checkSizes("2, 3, 4, 1") == False #El 1 es menor al 4, así que False
+    assert gest_Cafeteria.checkSizes("3, 2, 1") == False #Los elementos están en orden descendente, así que False
     
+#Test que comprueba que, la lista de tamaños, posea entre 1 y 5 elementos
 def test_sizeSize():
-    assert gest_Cafeteria.checkSizes("1") == True
-    assert gest_Cafeteria.checkSizes("1, 2, 3, 4, 5") == True
-    assert gest_Cafeteria.checkSizes("1, 2, 3, 4, 5, 6") == False
-    assert gest_Cafeteria.checkSizes("") == False
-    assert gest_Cafeteria.checkSizes(" ") == False
-    
+    assert gest_Cafeteria.checkSizes("1") == True #Un solo elemento es válido, así que True
+    assert gest_Cafeteria.checkSizes("1, 2, 3, 4, 5") == True #5 elementos es válido, así que True
+    assert gest_Cafeteria.checkSizes("1, 2, 3, 4, 5, 6") == False #6 elementos no es válido, así que False
+    assert gest_Cafeteria.checkSizes("") == False #Un string vacío no es válido, así que False
+    assert gest_Cafeteria.checkSizes(" ") == False #Un string con solo espacios en blanco no es válido, así que False
+
+#Test que comprueba que, el formato de la entrada, tenga el formato correcto
 def test_listFormat():
-    assert gest_Cafeteria.checkFormat("KawaFresa, 1, 3, 5") == True
-    assert gest_Cafeteria.checkFormat("KawaFresa, 1, 3, 5, , 6") == True
-    assert gest_Cafeteria.checkFormat("KawaFresa, 1, 3, 5,, 6") == True
-    assert gest_Cafeteria.checkFormat("1, 2, 3, 4, KawaFresa") == False
+    assert gest_Cafeteria.checkFormat("KawaFresa, 1, 3, 5") == True #El nombre es la primera entrada y, después, está una lista 
+                                                                    #de tamaños válidos, así que True
+    assert gest_Cafeteria.checkFormat("KawaFresa, 1, 3, 5, , 6") == True #Los espacios en blanco son ignorados, así que True
+    assert gest_Cafeteria.checkFormat("KawaFresa, 1, 3, 5,, 6") == True #2 comas seguidas son ignoradas, así que True
+    assert gest_Cafeteria.checkFormat("1, 2, 3, 4, KawaFresa") == False #La lista de tamaños es la primera entrada y, después,
+                                                                        #está el nombre, así que False
+    assert gest_Cafeteria.checkFormat("1, KawaFresa, 2, 3, 4") == False #El nombre se encuentra en medio de la lista de tamaños,
+                                                                        #así que False
     
